9bda28f8339da9be42dc4c559fb3273b7cf61277
maver <kentucky@posteo.de>
2023-06-25 22:01:04 +0200

    Remove doubled initialization of prompt manager

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -2037,7 +2037,6 @@ async function onModelChange() {
     if ($(this).is('#model_claude_select')) {
         console.log('Claude model changed to', value);
         oai_settings.claude_model = value;
-        //setupOpenAIPromptManager(openai_settings);
     }
 
     if ($(this).is('#model_windowai_select')) {
@@ -2048,7 +2047,6 @@ async function onModelChange() {
     if ($(this).is('#model_openai_select')) {
         console.log('OpenAI model changed to', value);
         oai_settings.openai_model = value;
-        setupOpenAIPromptManager(openai_settings);
     }
 
     if ($(this).is('#model_openrouter_select')) {
